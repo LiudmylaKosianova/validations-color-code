b47f3a437f9a8b67bdbb00d911655fd78872af84
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-16 07:43:45 +0200

    some changes

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -9,14 +9,20 @@ public class ColorCodeValidation {
 
         String regex1 = "#.*";//check if the color starts with #
         Pattern hashtagAtStart = Pattern.compile(regex1);
-        String regex3 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";
+        //String regex3 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";//works
+        //String regex3 = "[0-9a-fA-F]{3}";//doesn't work
+        //String regex3 = "([0-9a-fA-F]){3}";//doesn't work
+        String regex3 = "#{1}[0-9a-fA-F]{3}";
         Pattern colorHex3 = Pattern.compile(regex3);
-        String regex6 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";
+        //String regex6 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";//works
+        //String regex6 = "[0-9a-fA-F]{6}";//doesn't work
+        //String regex6 = "([0-9a-fA-F]){6}"//doesn't work
+        String regex6 = "#{1}[0-9a-fA-F]{6}";
         Pattern colorHex6 = Pattern.compile(regex6);
         if (color == null || !hashtagAtStart.matcher(color).matches()){
             return false;
         }else{
-            //return true;
+
             boolean three = colorHex3.matcher(color).matches();
             boolean six = colorHex6.matcher(color).matches();
             return three || six;
