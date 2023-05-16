a377c9a00feebabd39e159a4de1e9c32332653cb
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-16 07:29:23 +0200

    done

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -6,7 +6,21 @@ import java.util.regex.Pattern;
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
 
-        // Put your code here
+
+        String regex1 = "#.*";//check if the color starts with #
+        Pattern hashtagAtStart = Pattern.compile(regex1);
+        String regex3 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";
+        Pattern colorHex3 = Pattern.compile(regex3);
+        String regex6 = "#[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]";
+        Pattern colorHex6 = Pattern.compile(regex6);
+        if (color == null || !hashtagAtStart.matcher(color).matches()){
+            return false;
+        }else{
+            //return true;
+            boolean three = colorHex3.matcher(color).matches();
+            boolean six = colorHex6.matcher(color).matches();
+            return three || six;
+        }
 
     }
 }
